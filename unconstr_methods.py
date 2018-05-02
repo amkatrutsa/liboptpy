1de56508a2a663680bb7e0c2743af0f49166d259
Alexandr Katrutsa <aleksandr.katrutsa@phystech.edu>
2018-05-02 20:56:11 +0300

    Add Brazilai-Borwein method

--- unconstr_methods.py
+++ unconstr_methods.py
@@ -7,7 +7,8 @@ class DescentMethod(object):
         self.convergence = []
         self._f = f
         self._grad = grad
-        step_size.assign_function(f, grad)
+        if step_size is not None:
+            step_size.assign_function(f, grad)
         self._step_size = step_size
         self._par = kwargs
         
@@ -68,3 +69,47 @@ class Newton(DescentMethod):
         else:
             h = np.linalg.solve(hess, -grad)
         return h
+    
+class BB_method(DescentMethod):
+    def __init__(self, f, grad, **kwargs):
+        super().__init__(f, grad, None, **kwargs)
+    
+    def get_descent_direction(self, x):
+        return -self._grad(x)
+    
+    def solve(self, x0, max_iter=100, tol=1e-6, disp=False):
+        self.convergence = []
+        x_prev = x0.copy()
+        self.convergence.append(x_prev)
+        iteration = 0
+        print(self._par["init_alpha"])
+        while True:
+            h = self.get_descent_direction(x_prev)
+            if iteration == 0:
+                alpha = self._par["init_alpha"]
+            else:
+                alpha = self.get_stepsize(s, h_prev - h, self._par["type"])
+            x_next = x_prev + alpha * h
+            h_prev = h.copy()
+            s = x_next - x_prev
+            x_prev = x_next
+            self.convergence.append(x_next)
+            iteration += 1
+            if disp > 1:
+                print("Iteration {}/{}".format(iteration, max_iter))
+                print("Current function val =", self._f(x_next))
+                print("Current gradient norm = ", np.linalg.norm(self._grad(x_next)))
+            if self.check_convergence(tol) or iteration >= max_iter:
+                break
+        if disp:
+            print("Convergence in {} iterations".format(iteration))
+            print("Norm of gradient = {}".format(np.linalg.norm(self._grad(x_next))))
+            print("Function value = {}".format(self._f(x_next)))
+        return x_next
+    
+    def get_stepsize(self, s, g, alpha_type):
+        if alpha_type == 1:
+            alpha = g.dot(s) / g.dot(g)
+        elif alpha_type == 2:
+            alpha = s.dot(s) / g.dot(s)
+        return alpha
