9241b50dd404c250e37624d120169cd707ba21a7
Alexandr Katrutsa <aleksandr.katrutsa@phystech.edu>
2018-05-02 19:54:12 +0300

    Add Newton

--- unconstr_methods.py
+++ unconstr_methods.py
@@ -1,43 +1,18 @@
 import numpy as np
 
-__all__ = ["GradientDescent"]
+__all__ = ["GradientDescent", "Newton"]
 
-class _DescentMethod(object):
-    def __init__(self):
+class DescentMethod(object):
+    def __init__(self, f, grad, step_size, **kwargs):
         self.convergence = []
-    
+        self._f = f
+        self._grad = grad
+        step_size.assign_function(f, grad)
+        self._step_size = step_size
+        self._par = kwargs
+        
     def get_convergence(self):
         return self.convergence
-    
-    def solve(self, x0, max_iter=100, tol=1e-6):
-        raise NotImplementedError("Method solve has to be implemented!")
-    
-    def get_descent_direction(self, x):
-        raise NotImplementedError("You have to provide method for descent direction!")
-        
-    def check_convergence(self, tol):
-        raise NotImplementedError("You have to stopping criterion to check convergence!")
-        
-    def get_stepsize(self, h):
-        raise NotImplementedError("You have to provide method to set stepsize!")
-        
-class GradientDescent(_DescentMethod):
-    def __init__(self, f, grad, step_size, **kwargs):
-        super().__init__()
-        self.f = f
-        self.grad = grad
-        self.par = kwargs
-        step_size.assign_function(f, grad)
-        self.step_size = step_size
-        
-    def check_convergence(self, tol):
-        return np.linalg.norm(self.grad(self.convergence[-1])) < tol
-    
-    def get_descent_direction(self, x):
-        return -self.grad(x)
-    
-    def get_stepsize(self, h):
-        return self.step_size(h, self.convergence[-1], **self.par)
     
     def solve(self, x0, max_iter=100, tol=1e-6, disp=False):
         self.convergence = []
@@ -46,18 +21,50 @@ class GradientDescent(_DescentMethod):
         iteration = 0
         while True:
             h = self.get_descent_direction(x)
-            alpha = self.step_size.get_stepsize(h, x)
+            alpha = self.get_stepsize(h)
             x = x + alpha * h
             self.convergence.append(x)
             iteration += 1
             if disp > 1:
                 print("Iteration {}/{}".format(iteration, max_iter))
-                print("Current function val =", self.f(x))
-                print("Current gradient norm = ", np.linalg.norm(self.grad(x)))
+                print("Current function val =", self._f(x))
+                print("Current gradient norm = ", np.linalg.norm(self._grad(x)))
             if self.check_convergence(tol) or iteration >= max_iter:
                 break
         if disp:
             print("Convergence in {} iterations".format(iteration))
-            print("Norm of gradient = {}".format(np.linalg.norm(self.grad(x))))
-            print("Function value = {}".format(self.f(x)))
+            print("Norm of gradient = {}".format(np.linalg.norm(self._grad(x))))
+            print("Function value = {}".format(self._f(x)))
         return x
+    
+    def get_descent_direction(self, x):
+        raise NotImplementedError("You have to provide method for descent direction!")
+        
+    def check_convergence(self, tol):
+        return np.linalg.norm(self._grad(self.convergence[-1])) < tol
+        
+    def get_stepsize(self, h):
+        return self._step_size.get_stepsize(h, self.convergence[-1], **self._par)
+        
+class GradientDescent(DescentMethod):
+    def __init__(self, f, grad, step_size, **kwargs):
+        super().__init__(f, grad, step_size, **kwargs)
+    
+    def get_descent_direction(self, x):
+        return -self._grad(x)
+    
+    
+class Newton(DescentMethod):
+    def __init__(self, f, grad, hess, step_size, linsolver=None, **kwargs):
+        super().__init__(f, grad, step_size, **kwargs)
+        self._hess = hess
+        self._linsolver = linsolver
+    
+    def get_descent_direction(self, x):
+        grad = self._grad(x)
+        hess = self._hess(x)
+        if self._linsolver:
+            h = self._linsolver(hess, -grad)
+        else:
+            h = np.linalg.solve(hess, -grad)
+        return h
