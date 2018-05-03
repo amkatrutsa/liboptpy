9938e26ea04e32de20fa3a0c986cc8d453d1cad1
Alexandr Katrutsa <aleksandr.katrutsa@phystech.edu>
2018-05-03 11:27:35 +0300

    Add cg method FR and quadratic

--- unconstr_methods.py
+++ unconstr_methods.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-__all__ = ["GradientDescent", "Newton"]
+__all__ = ["GradientDescent", "Newton", "BB_method", "ConjugateGradientQuad",
+           ""]
 
 class DescentMethod(object):
     def __init__(self, f, grad, step_size, **kwargs):
@@ -112,3 +113,48 @@ class BB_method(DescentMethod):
         elif alpha_type == 2:
             alpha = s.dot(s) / g.dot(s)
         return alpha
+    
+class ConjugateGradientQuad(DescentMethod):
+    def __init__(self, A, b):
+        f = lambda x: 0.5 * x.dot(A.dot(x)) - b.dot(x)
+        grad = lambda x: A.dot(x) - b
+        super().__init__(f, grad, None)
+        self._A = A
+        self._b = b
+        
+    def get_descent_direction(self, x):
+        if (len(self.convergence) == 1):
+            h = -self._grad(x)
+            self._h = h
+            self._r = -h
+        else:
+            r_next = self._r + self._alpha * self._A.dot(self._h)
+            beta = r_next.dot(r_next) / self._r.dot(self._r)
+            h = -r_next + beta * self._h
+            self._r = r_next
+            self._h = h
+        return h
+    
+    def get_stepsize(self, h):
+        alpha = self._r.dot(self._r) / h.dot(self._A.dot(h))
+        self._alpha = alpha
+        return alpha
+    
+class ConjugateGradientFR(DescentMethod):
+    def __init__(self, f, grad, step_size, restart=None, **kwargs):
+        super().__init__(f, grad, step_size, **kwargs)
+        if restart is not None:
+            restart.assign_function(f, grad)
+        self._restart = restart
+        
+    def get_descent_direction(self, x):
+        if (len(self.convergence) == 1) or (self._restart is not None and 
+                                            self._restart(len(self.convergence), x)):
+            h = -self._grad(x)
+            self._h = h
+        else:
+            current_grad = self._grad(self.convergence[-1])
+            beta = current_grad.dot(current_grad) / self._h.dot(self._h)
+            h = -current_grad + beta * self._h
+            self._h = h
+        return h
