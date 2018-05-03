b89994ae6c8600a3dba6a178e2c5570334260861
Alexandr Katrutsa <aleksandr.katrutsa@phystech.edu>
2018-05-03 11:39:55 +0300

    Add module for wide-range utils

---
+++ utils.py
@@ -0,0 +1,13 @@
+class Restart(object):
+    def __init__(self, limit_dim):
+        self._dim = limit_dim
+    
+    def __call__(self, num_iter, x):
+        if num_iter % self._dim == 0:
+            return True
+        else:
+            return False
+    
+    def assign_function(self, f, grad):
+        self._f = f
+        self._grad = grad
